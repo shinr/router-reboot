313666d99c27caae31fddf2b1b9a193ed1d3d28f
shinr <jukka.lansineva@student.oulu.fi>
2015-10-29 22:37:26 +0200

    python 3 + multithreading

--- router-reboot.py
+++ router-reboot.py
@@ -8,25 +8,41 @@ import os
 import ping
 import time
 import sys
+import threading
+import queue
 
 target_host = ''
 router_host = ''
 failed_packets = 0
+reboot_limit = 4
 wait_delay = 84
 
 user_name = ''
 password = ''
 
+def socket_loop():
+	reached = ping_host(target_host)
+	if reached:
+		failed_packets = 0
+	else:
+		failed_packets += 1
+
+	if failed_packets > reboot_limit:
+		reboot_router()
+	else:
+		print ("current failed packets = ", failed_packets)
+	time.sleep(wait_delay)
+
 def ping_host(host):
-	print "pinging " + host + "... ",
+	print ("pinging ", host, "... ", end="")
 	if ping.verbose_ping(host):
-		print "reached OK, ",
+		print ("reached OK, ", end="")
 		return True
-	print "not reached, ",
+	print ("not reached, ", end="")
 	return False
 
 def reboot_router():
-	print "rebooting "+ router_host + "..."
+	print ("rebooting ", router_host, "...")
 	try:
 		connection = telnetlib.Telnet(router_host)
 		connection.read_until("DD-WRT login: ")
@@ -39,23 +55,23 @@ def reboot_router():
 		connection.close()
 	except EOFError:
 		return
-if len(sys.argv) < 5:
-	print "Usage: router-reboot.py [router-ip] [target-ip] [router-username] [router-password]"
-	sys.exit()
-user_name = sys.argv[3]
-password = sys.argv[4]
-target_host = sys.argv[2]
-router_host = sys.argv[1]
 
-while True:
-	reached = ping_host(target_host)
-	if reached:
-		failed_packets = 0
-	else:
-		failed_packets += 1
+if __name__ == "__main__":		
+	if len(sys.argv) < 5:
+		print ("Usage: router-reboot.py [router-ip] [target-ip] [router-username] [router-password]")
+		sys.exit()
 
-	if failed_packets > 4:
-		reboot_router()
-	else:
-		print "current failed packets = %d" % failed_packets
-	time.sleep(wait_delay)
+	user_name = sys.argv[3]
+	password = sys.argv[4]
+	target_host = sys.argv[2]
+	router_host = sys.argv[1]
+
+	connections = threading.Thread(target=socket_loop, name="connections")
+	connections.daemon = True
+	connections.start()
+
+	message_queue = queue.Queue()
+
+	while True:
+		user_command = raw_input()
+	
